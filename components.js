184caae33b1ae43feee8d43799c526a6f6619d40
Luke Dixon <github@lukedixon.nl>
2023-01-11 20:35:45 +0100

    finished with 3 object

--- components.js
+++ components.js
@@ -1,17 +1,69 @@
-AFRAME.registerComponent('foo', {
+AFRAME.registerComponent('reloader', {
   init: function () {
-    const plane = document.getElementById('plane');
-    let quote;
-    fetch('https://swapi.dev/api/planets/1/')
-      .then((res) => res.json())
-      .then((data) => {
-        quote = data;
-      });
-    plane.addEventListener('mouseenter', function (e) {
-      console.log(quote);
-    });
+    const planets = document.querySelector('.planets');
+
+    const BASE_URL = 'https://swapi.dev/api/planets/';
+    this.newPlanet = function () {
+      let randomNum = Math.floor(Math.random() * 60) + 1;
+      fetch(BASE_URL + randomNum)
+        .then((response) => response.json())
+        .then((data) => {
+          planets.setAttribute('text', 'value', data.name);
+        });
+    };
+    this.el.sceneEl.addEventListener('click', this.newPlanet);
   },
-  update: function () {},
+  update: function () {
+    this.newPlanet();
+  },
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {},
+});
+
+AFRAME.registerComponent('film', {
+  init: function () {
+    const film = document.querySelector('.film');
+    const BASE_URL = 'https://swapi.dev/api/films/';
+    this.newFilm = function () {
+      let randomNum = Math.floor(Math.random() * 5) + 1;
+      fetch(BASE_URL + randomNum)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          film.setAttribute('text', 'value', data.title);
+        });
+    };
+    this.el.addEventListener('mouseenter', this.newFilm);
+  },
+  update: function () {
+    this.newFilm();
+  },
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {},
+});
+
+AFRAME.registerComponent('vehicles', {
+  init: function () {
+    const vehicles = document.querySelector('.vc');
+    const BASE_URL = 'https://swapi.dev/api/vehicles/';
+    this.newVehicles = function () {
+      let randomNum = Math.floor(Math.random() * 39) + 1;
+      fetch(BASE_URL + randomNum)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          vehicles.setAttribute('text', 'value', data.name);
+        });
+    };
+    this.el.addEventListener('mouseleave', this.newVehicles);
+  },
+  update: function () {
+    this.newVehicles();
+  },
   tick: function () {},
   remove: function () {},
   pause: function () {},
